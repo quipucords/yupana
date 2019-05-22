41e531250356be6c5ad7cda9db7febbd8817137f
abaiken <aaiken@redhat.com>
2019-05-22 10:53:27 -0400

    change 'hash' to 'request_id'

--- yupana/processor/report_processor.py
+++ yupana/processor/report_processor.py
@@ -217,7 +217,7 @@ class ReportProcessor(AbstractProcessor):  # pylint: disable=too-many-instance-a
             account_number=self.account_number, report_platform_id=self.report_platform_id))
         try:
             print('The upload message is: %s' % str(self.upload_message))
-            message_hash = self.upload_message['hash']
+            message_hash = self.upload_message['request_id']
             await self._send_confirmation(message_hash)
             self.next_state = Report.VALIDATION_REPORTED
             options = {'ready_to_archive': True}
