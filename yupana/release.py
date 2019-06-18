36a0cfda3165e4ec6d1b98ea8c4a5045ee2f78ef
Ashley Aiken <aaiken@redhat.com>
2019-06-18 18:30:54 -0400

    Addition of Deployment Version  (#166)

--- yupana/release.py
+++ yupana/release.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2018-2019 Red Hat, Inc.
+# Copyright 2019 Red Hat, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,26 +14,13 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
+"""File to hold release constants."""
+import re
 
-"""Serializer to capture server status."""
+BUILD_VERSION = 'BUILD_VERSION_PLACEHOLDER'
+DEFAULT_VERSION = '0.0.0'
 
-from rest_framework import serializers
-
-from .model import Status
-
-
-class StatusSerializer(serializers.Serializer):  # pylint:disable=abstract-method
-    """Serializer for the Status model."""
-
-    api_version = serializers.IntegerField()
-    release_version = serializers.CharField()
-    git_commit = serializers.CharField()
-    modules = serializers.DictField()
-    platform_info = serializers.DictField()
-    python_version = serializers.CharField()
-
-    class Meta:
-        """Metadata for the serializer."""
-
-        model = Status
-        fields = '__all__'
+# pylint: disable=anomalous-backslash-in-string
+VERSION_PATTERN = re.compile('\d+\.\d+\.\d+')  # noqa: W605 (invalid-escape-sequence)
+if not VERSION_PATTERN.match(BUILD_VERSION):
+    BUILD_VERSION = DEFAULT_VERSION
