f4d0007548852adaed2608bf1ea4ebc96d111317
Ryan Himmelwright <ryan.himmelwright@gmail.com>
2019-10-22 15:45:05 -0400

    Updated the services defined for the yupana smoke tests (#257)
    * Updated the services defined for the yupana smoke tests

--- Jenkinsfile-smoke.groovy
+++ Jenkinsfile-smoke.groovy
@@ -12,13 +12,13 @@ if (env.CHANGE_ID) {
         // the service-set/component for this app in e2e-deploy "buildfactory"
         ocDeployerBuilderPath: "subscriptions/yupana",
         // the service-set/component for this app in e2e-deploy "templates"
-        ocDeployerComponentPath: "subscriptions/yupana",
+        ocDeployerComponentPath: "subscriptions",
         // the service sets to deploy into the test environment
-        ocDeployerServiceSets: "upload-service,insights-inventory,subscriptions",
+        ocDeployerServiceSets: "platform,platform-mq,subscriptions",
         // the iqe plugins to install for the test
         iqePlugins: ["iqe-yupana-plugin"],
         // the pytest marker to use when calling `iqe tests all`
-        pytestMarker: "yupana-smoke",
+        pytestMarker: "yupana_smoke",
         // optional id for an IQE configuration file stored as a secret in Jenkins
         //configFileCredentialsId: "myId",
     )
