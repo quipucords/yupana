1a681b7b503f0749fa6bb1a18bc69b0941794f3f
Sanket Jagtap <san7ketjagtap@gmail.com>
2021-08-16 14:24:59 +0530

    Update Jenkinsfile-smoke.groovy

--- Jenkinsfile-smoke.groovy
+++ Jenkinsfile-smoke.groovy
@@ -16,7 +16,7 @@ if (env.CHANGE_ID) {
         // the service sets to deploy into the test environment
         ocDeployerServiceSets: "ingress,inventory,platform-mq,subscriptions",
         // the iqe plugins to install for the test
-        iqePlugins: ["iqe-foreman-rh-cloud-plugin"],
+        iqePlugins: ["foreman-rh-cloud"],
         // the pytest marker to use when calling `iqe tests all`
         pytestMarker: "core",
         // optional id for an IQE configuration file stored as a secret in Jenkins
